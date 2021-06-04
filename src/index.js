7be00165c89f509ff8cdaddf4e18da08e2677a9f
typhonrt <support@typhonrt.org>
2021-06-04 04:22:02 -0700

    explicit plugin export

--- src/index.js
+++ src/index.js
@@ -2,6 +2,5 @@ import ErrorParser                     from './plugin/ErrorParser.js';
 
 export { default as defaultNamespace } from './data/defaultNamespace.js';
 export { default as normalizeError }   from './util/normalizeError.js';
-export { ErrorParser };
 
-export default new ErrorParser();
+export default ErrorParser;
---
+++ src/plugin.js
@@ -0,0 +1,4 @@
+import ErrorParser   from './plugin/ErrorParser.js';
+
+// Provide a default instance as a plugin.
+export default new ErrorParser();
